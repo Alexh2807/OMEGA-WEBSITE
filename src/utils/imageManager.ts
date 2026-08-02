2384221ed644d5a83704883fc4eb4bcb2c368987
Alexh2807 <alexishidalgo34000@gmail.com>
2026-08-02 16:23:10 +0200

    Revert "Boitier OMEGA DMX : modele 3D orientable et illustration vectorielle"
    This reverts commit 29bca783e87bb7beaa1f758781e393bb3994f9f7.

--- src/utils/imageManager.ts
+++ src/utils/imageManager.ts
@@ -4,7 +4,7 @@
  * ⚠️ CE FICHIER EST GÉNÉRÉ AUTOMATIQUEMENT
  * Ne pas modifier manuellement - Exécutez: npm run scan-images
  *
- * Dernière mise à jour: 02/08/2026 15:51:11
+ * Dernière mise à jour: 02/08/2026 16:23:10
  */
 
 // Liste de toutes les images disponibles dans /public
